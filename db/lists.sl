5cd5c42a62503b4c73bdf00cd0348f3d50dd05d1
YouSeeAllStudiosPublic <ormrnar@gmail.com>
2020-09-11 14:54:41 +1200

    implemented delete from user db

--- db/lists.sl
+++ db/lists.sl
@@ -2,10 +2,11 @@
 item1
 item2
 item3
+item4
 %
 #bob
 item5
-item6
 item7
 item8
+item9
 %
--- src/include/list.h
+++ src/include/list.h
@@ -20,11 +20,13 @@ class List{
     vector<vector<string>> mainList;
     vector<string> list;
     string name;
+    int currentUserIndex;
 
     // function decleration.
     void print_menu();
     void print_list();
     void add_item();
     void delete_item();
-    void find_userList();
+    bool find_userList();
+    void save_list();
 };
